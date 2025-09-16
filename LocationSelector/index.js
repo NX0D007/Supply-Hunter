75d31d58fd015eeabe92f3be79f96f7db1154104
NX0D007 <laknadir@yahoo.fr>
2025-09-16 20:55:53 +0100

    Delete LocationSelector directory

--- LocationSelector/index.js
+++
@@ -1,4 +0,0 @@
-export { state, populateCountries, populateLocationTypes, cleanDepartmentName } from './LocationManager.js';
-export { renderCheckboxes } from './LocationDropdown.js';
-export { updateTags } from './LocationTags.js';
-export { initSearch } from './LocationSearch.js';
