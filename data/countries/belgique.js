bc39063af696e590be3a270a2dd8dafd250e164e
NX0D007 <laknadir@yahoo.fr>
2025-09-16 20:38:50 +0100

    Create belgique.js

---
+++ data/countries/belgique.js
@@ -0,0 +1,26 @@
+const Belgique = {
+    name: "Belgique",
+    regions: [
+        "Région flamande",
+        "Région wallonne",
+        "Région de Bruxelles-Capitale"
+    ],
+    provinces: [
+        "Anvers",
+        "Brabant flamand",
+        "Brabant wallon",
+        "Flandre-Occidentale",
+        "Flandre-Orientale",
+        "Hainaut",
+        "Liège",
+        "Limbourg",
+        "Luxembourg",
+        "Namur"
+    ],
+    locationTypes: [
+        { value: 'region', label: 'Région' },
+        { value: 'province', label: 'Province' }
+    ]
+};
+
+export default Belgique;
